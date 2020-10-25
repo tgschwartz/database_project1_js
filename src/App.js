e6963444a1f8267c57d6eb82c6ee91da94345abc
Tova Schwartz <tovagschwartz@gmail.com>
2020-10-24 23:14:38 -0400

    completed UI

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./styles.css";
 
 import { useImmer } from "use-immer";
@@ -6,39 +6,31 @@ import { useImmer } from "use-immer";
 const DressesApp = () => {
   const [appState, updateState] = useImmer({
     filterType: "all",
-    dresses: [
-      { id: 0, name: "uno", size: 2, returned: true },
-      { id: 1, name: "dos", size: 4, returned: false }
-    ],
-    customers: [
-      {
-        id: 8,
-        first_name: "Tova",
-        last_name: "Schwartz",
-        phone: '1234321',
-        size: 1,
-        wedding_date: "04/03/2021"
-      }
-    ]
+    dresses: [],
+    customers: [],
+    reservations: []
   });
 
-  const Dress = ({ id, name, size, returned, toggleDress }) => {
+  // Loads dress info already in database
+  useEffect(() => {
+    fetch("http://localhost:5000/dresses")
+      .then(response => response.json())
+      .then(json => {
+        updateState(draft => {
+          draft.dresses = json.dresses;
+        });
+      });
+  }, []);
+
+  // CreateDress
+  const Dress = ({id, dress_name, dress_size, available}) => {
     return (
       <li
-        onClick={toggleDress}
         style={{
-          textDecoration: returned ? "line-through" : "none"
+          textDecoration: available ? "line-through" : "none"
         }}
       >
-        {"Dress: " + name + " | size: " + size}
-      </li>
-    );
-  };
-
-  const Customer = ({ id, first_name, last_name, phone, size, wedding_date}) => {
-    return (
-      <li>
-        {"ID: " + id + ", Name: " + first_name + " " + last_name}
+        {"ID: " + id + " | Dress: " + dress_name + ", Size: " + dress_size}
       </li>
     );
   };
@@ -61,8 +53,162 @@ const DressesApp = () => {
         <br></br>
         <input ref={(r) => (dressSizeInput = r)} type="text" placeholder="Dress Size"/>
         <br></br>
-        <input type="submit" value="Submit" />
+        <input type="submit" value="Add Dress" />
       </form>
+    );
+  };
+
+  const createDressAPI = (dress_name, dress_size) => {
+    return fetch("http://localhost:5000/createDress", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        dress_name: dress_name,
+        dress_size: dress_size
+      })
+    })
+      .then(response => response.json())
+      .then(json => json.dress);
+  };
+
+  const createDress = (name, size) => {
+    createDressAPI(name, size).then(dress => {
+      updateState((draft) => {
+        draft.dresses.push({
+          id: dress.id,
+          dress_name: dress.dress_name,
+          dress_size: dress.dress_size,
+          available: dress.available
+        });
+      });
+    })
+  };
+
+  const ReturnedFilter = ({ filterType, setFilterType }) => (
+    <span>
+      {["all", "available", "rented"].map((status, i) => {
+        return (
+          <button
+            onClick={() => setFilterType(status)}
+            disabled={appState.filterType === status}
+          >
+            {status}
+          </button>
+        );
+      })}
+    </span>
+  );
+
+  const filteredDresses = () => {
+    if (appState.filterType === "all") {
+      return appState.dresses;
+    } else if (appState.filterType === "rented") {
+      return appState.dresses.filter((dress) => dress.available === 0);
+    } else if (appState.filterType === "available") {
+      return appState.dresses.filter((dress) => dress.available === 1);
+    }
+  };
+
+  const setFilterType = (filterType) => {
+    updateState((draft) => {
+      draft.filterType = filterType;
+    });
+  };
+
+  // Load reservations info already in database
+  useEffect(() => {
+    fetch("http://localhost:5000/reservations")
+      .then(response => response.json())
+      .then(json => {
+        updateState(draft => {
+          draft.reservationss = json.reservations;
+        });
+      });
+  }, []);
+
+  // Create Reservation
+  const Reservation = ({id, customer_id, dress_id}) => {
+    return (
+      <li>
+        {"ID: " + id + " | Dress: " + dress_id + ", customer: " + customer_id}
+      </li>
+    );
+  };
+
+  const ReservationForm = ({ reservationAction }) => {
+    // variable to hold a reference to the input
+    let reservationDressName;
+    let reservationCustomerID;
+    let reservationRentDate;
+
+    const handleSubmitReservation = (event) => {
+      event.preventDefault();
+      reservationAction(reservationDressName.value,
+                        reservationCustomerID.value,
+                        reservationRentDate.value);
+      reservationDressName.value = "";
+      reservationCustomerID.value = "";
+      reservationRentDate.value = "";
+    };
+
+    return (
+      <form onSubmit={handleSubmitReservation}>
+      <input ref={(r) => (reservationDressName = r)} type="text" placeholder="Dress ID"/>
+      <br></br>
+      <input ref={(r) => (reservationCustomerID = r)} type="text" placeholder="Customer ID"/>
+      <br></br>
+      <input ref={(r) => (reservationRentDate = r)} type="text" placeholder="Date: YYYY/MM/DD"/>
+      <br></br>
+      <input type="submit" value="Reserve" />
+    </form>     
+    );
+  };
+
+
+  const createReservationAPI = (customer_id, dress_id, rent_date) => {
+    return fetch("http://localhost:5000/reservations", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        customer_id: customer_id,
+        dress_id: dress_id,
+        rent_date: rent_date
+      })
+    })
+      .then(response => response.json())
+      .then(json => json.customer);
+  };
+
+  const createReservation = (customer_id, dress_id, rent_date) => {
+    createReservationAPI(customer_id, dress_id, rent_date).then(reservation => {
+      updateState((draft) => {
+        draft.reservations.push({
+          id: reservation.id,
+          customer_id: reservation.customer_id,
+          dress_id: reservation.dress_id,
+          rent_date: reservation.rent_date
+        });
+      });
+    })
+  };
+
+  // Load customer info already in database
+  useEffect(() => {
+    fetch("http://localhost:5000/customers")
+      .then(response => response.json())
+      .then(json => {
+        updateState(draft => {
+          draft.customers = json.customers;
+        });
+      });
+  }, []);
+
+  // Create Customer
+  const Customer = ({id, first_name, last_name}) => {
+    return (
+      <li>
+        {"ID: " + id + " | Name: " + first_name + " " + last_name}
+      </li>
     );
   };
 
@@ -98,77 +244,124 @@ const DressesApp = () => {
       <br></br>
       <input ref={(r) => (customerSizeInput = r)} type="text" placeholder="Size"/>
       <br></br>
-      <input ref={(r) => (customerDateInput = r)} type="text" placeholder="Wedding Date: MM/DD/YYYY"/>
+      <input ref={(r) => (customerDateInput = r)} type="text" placeholder="Wedding: YYYY/MM/DD"/>
       <br></br>
       <input type="submit" value="Submit" />
     </form>     
     );
   };
 
-  const createDress = (name, size) => {
-    updateState((draft) => {
-      draft.dresses.push({
-        id: draft.dresses.length,
-        name: name,
-        size: size,
-        returned: false
-      });
-    });
-  };
 
-  const createCustomer = (first_name, last_name, phone, size, wedding_date) => {
-    updateState((draft) => {
-      draft.customers.push({
-        id: draft.dresses.length,
+  const createCustomerAPI = (first_name, last_name, phone_number, size, wedding_date) => {
+    return fetch("http://localhost:5000/customers", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
         first_name: first_name,
         last_name: last_name,
-        phone: phone,
+        phone_number: phone_number,
         size: size,
         wedding_date: wedding_date
+      })
+    })
+      .then(response => response.json())
+      .then(json => json.customer);
+  };
+
+  const createCustomer = (first_name, last_name, phone_number, size, wedding_date) => {
+    createCustomerAPI(first_name, last_name, phone_number, size, wedding_date).then(customer => {
+      updateState((draft) => {
+        draft.customers.push({
+          id: customer.id,
+          first_name: customer.first_name,
+          last_name: customer.last_name
+        });
       });
-    });
+    })
   };
 
-  const toggleDress = (id) => {
-    updateState((draft) => {
-      if (draft.dresses[id].returned === false) {
-        draft.dresses[id].returned = true;
-      } else if (draft.dresses[id].returned === true) {
-        draft.dresses[id].returned = false;
-      }
-    });
+
+  // Return
+  const ReturnDressForm = ({ returnDressAction }) => {
+    // variable to hold a reference to the input
+    let returnReservationID;
+    let returnReturnOn;
+
+    const handleSubmitReturnDress = (event) => {
+      event.preventDefault();
+      returnDressAction(returnReservationID.value, returnReturnOn.value);
+      returnReservationID.value = "";
+      returnReturnOn.value = "";
+    };
+
+    return (
+      <form onSubmit={handleSubmitReturnDress}>
+      <input ref={(r) => (returnReservationID = r)} type="text" placeholder="Reservation ID"/>
+      <br></br>
+      <input ref={(r) => (returnReturnOn = r)} type="text" placeholder="Date: YYYY/MM/DD"/>
+      <br></br>
+      <input type="submit" value="Return Dress" />
+    </form>     
+    );
   };
 
-  const ReturnedFilter = ({ filterType, setFilterType }) => (
-    <span>
-      {["all", "available", "rented"].map((status, i) => {
-        return (
-          <button
-            onClick={() => setFilterType(status)}
-            disabled={appState.filterType === status}
-          >
-            {status}
-          </button>
-        );
-      })}
-    </span>
-  );
 
-  const filteredDresses = () => {
-    if (appState.filterType === "all") {
-      return appState.dresses;
-    } else if (appState.filterType === "rented") {
-      return appState.dresses.filter((dress) => dress.returned === true);
-    } else if (appState.filterType === "available") {
-      return appState.dresses.filter((dress) => dress.returned === false);
-    }
+  const returnDressAPI = (reservation_id, return_on) => {
+    return fetch("http://localhost:5000/update", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({
+        reservation_id: reservation_id,
+        return_on: return_on
+      })
+    })
+      .then(response => response.json())
+      .then(json => json.dress_id);
   };
 
-  const setFilterType = (filterType) => {
-    updateState((draft) => {
-      draft.filterType = filterType;
-    });
+  const returnDress = (reservation_id, return_on) => {
+    returnDressAPI(reservation_id, return_on).then(dress_id => {
+      updateState((draft) => {
+        const dressToUpdate = draft.dresses.find(d => d.id === dress_id);
+        dressToUpdate.available = 1;
+      });
+    })
   };
+
+  // const toggleTaskAPI = (id, completed) => {
+  //   return fetch(`http://localhost:5000/tasks/${id}`, {
+  //     method: "PUT",
+  //     headers: { "content-type": "application/json" },
+  //     body: JSON.stringify({
+  //       completed: completed
+  //     })
+  //   })
+  //     .then(response => response.json())
+  //     .then(json => json.task);
+  // };
+
+  // const toggleTask = id => {
+  //   const task = appState.tasks.find(t => t.id === id);
+
+  //   toggleTaskAPI(id, !task.completed).then(task => {
+  //     updateState(draft => {
+  //       const taskToUpdate = draft.tasks.find(t => t.id === id);
+  //       taskToUpdate.completed = task.completed;
+  //     });
+  //   });
+  // };
+
+  // const toggleDress = (id) => {
+  //   updateState((draft) => {
+  //     if (draft.dresses[id].returned === false) {
+  //       draft.dresses[id].returned = true;
+  //     } else if (draft.dresses[id].returned === true) {
+  //       draft.dresses[id].returned = false;
+  //     }
+  //   });
+  // };
+
+  
 
   // uncomment out JSX blocks as you solve each problem
   // to uncomment, remove the enclosing '{/*' and '*/}'
@@ -176,6 +369,11 @@ const DressesApp = () => {
     <div>
       <h3> Add a Dress </h3>
       {<DressForm dressAction={createDress} />}
+      <h3> Add a Customer </h3>
+      {<CustomerForm customerAction={createCustomer} />}
+      <h3> Manage Reservations </h3>
+      {<ReservationForm reservationAction={createReservation} />}
+      {<ReturnDressForm returnDressAction={returnDress} />}
 
       <h3> Dresses </h3>
       {
@@ -188,16 +386,13 @@ const DressesApp = () => {
         {filteredDresses().map((dress) => (
           <Dress
             id={dress.id}
-            name={dress.name}
-            size={dress.size}
-            returned={dress.returned}
-            toggleDress={() => toggleDress(dress.id)}
+            dress_name={dress.dress_name}
+            dress_size={dress.dress_size}
+            available={dress.available}
+            //toggleDress={() => toggleDress(dress.id)}
           />
         ))}
       </ul>
-
-      <h3> Add a Customer </h3>
-      {<CustomerForm customerAction={createCustomer} />}
 
       <h3> Customers </h3>
       <ul>
@@ -206,9 +401,19 @@ const DressesApp = () => {
             id={customer.id}
             first_name={customer.first_name}
             last_name={customer.last_name}
-            phone={customer.phone}
+            phone_number={customer.phone_number}
             size={customer.size}
             wedding_date={customer.wedding_date}/>
+        ))}
+      </ul>
+      
+      <h3> Reservations </h3>
+      <ul>
+        {appState.reservations.map((reservation) => (
+          <Reservation
+            id={reservation.id}
+            customer_id={reservation.customer_id}
+            dress_id={reservation.dress_id}/>
         ))}
       </ul>
     </div>
@@ -222,3 +427,4 @@ export default function App() {
     </div>
   );
 }
+
